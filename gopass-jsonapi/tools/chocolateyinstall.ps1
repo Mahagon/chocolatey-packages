223bf3656d7b3fce233e9f4a5793ee6acc43a4c7
Daniel Jacobs <mail@danieljacobs.de>
2022-12-07 19:41:48 +0100

    ⬆️ update to gopass-jsonapi 1.15.0
    Signed-off-by: Daniel Jacobs <mail@danieljacobs.de>

--- gopass-jsonapi/tools/chocolateyinstall.ps1
+++ gopass-jsonapi/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $ErrorActionPreference = 'Stop'
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/gopasspw/gopass-jsonapi/releases/download/v1.14.11/gopass-jsonapi-1.14.11-windows-amd64.zip'
+$url64      = 'https://github.com/gopasspw/gopass-jsonapi/releases/download/v1.15.0/gopass-jsonapi-1.15.0-windows-amd64.zip'
 $packageParameters = Get-PackageParameters
 
 if (!$packageParameters['BROWSERS']) { $packageParameters['BROWSERS'] = 'chrome' }
@@ -12,7 +12,7 @@ $packageArgs = @{
   unzipLocation  = $toolsDir
   url64bit       = $url64
   softwareName   = 'gopass-jsonapi*'
-  checksum64     = '7375353e0a1cea65d49f906658824111aad65100e25d5b36082a96220d60366b'
+  checksum64     = 'e4d4ffad006c51951a5f7c210c5e2cc6d014657ada0c62b3ad47a20f04c6f9e5'
   checksumType64 = 'sha256'
 }
 
