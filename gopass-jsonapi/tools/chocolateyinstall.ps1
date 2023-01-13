1ed2d3e0b1da36c8a0806b636699a9e935f0e7b1
Daniel Jacobs <mail@danieljacobs.de>
2023-01-13 14:52:23 +0100

    Revert "⬇️ return to gopass-jsonapi 1.5.0 for chocolatey approval"
    This reverts commit 8ab0dcfb2bca4c6e4c63a4ba0484012c4d759c4b.

--- gopass-jsonapi/tools/chocolateyinstall.ps1
+++ gopass-jsonapi/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $ErrorActionPreference = 'Stop'
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/gopasspw/gopass-jsonapi/releases/download/v1.15.0/gopass-jsonapi-1.15.0-windows-amd64.zip'
+$url64      = 'https://github.com/gopasspw/gopass-jsonapi/releases/download/v1.15.3/gopass-jsonapi-1.15.3-windows-amd64.zip'
 $packageParameters = Get-PackageParameters
 
 if (!$packageParameters['Browser']) { $packageParameters['Browser'] = 'chrome' }
@@ -13,7 +13,7 @@ $packageArgs = @{
   unzipLocation  = $toolsDir
   url64bit       = $url64
   softwareName   = 'gopass-jsonapi*'
-  checksum64     = 'e4d4ffad006c51951a5f7c210c5e2cc6d014657ada0c62b3ad47a20f04c6f9e5'
+  checksum64     = '71b5b288686a3f72fa17b92026ae69052f57e7288c728df0e6645688e7299b09'
   checksumType64 = 'sha256'
 }
 
